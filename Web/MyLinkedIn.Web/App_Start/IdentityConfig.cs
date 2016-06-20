137c10ddf4d3785a90aa0f35f2575a0d20e068de
jdimov59 <jdimov59@gmail.com>
2016-06-20 20:24:41 +0300

    Identity models refactored (extracted to libraries)

--- Web/MyLinkedIn.Web/App_Start/IdentityConfig.cs
+++ Web/MyLinkedIn.Web/App_Start/IdentityConfig.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using MyLinkedIn.Web.Models;
+using MyLinkedIn.DataModels;
+using MyLinkedIn.Data;
 
 namespace MyLinkedIn.Web
 {
--- Web/MyLinkedIn.Web/Controllers/AccountController.cs
+++ Web/MyLinkedIn.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MyLinkedIn.Web.Models;
+using MyLinkedIn.DataModels;
 
 namespace MyLinkedIn.Web.Controllers
 {
